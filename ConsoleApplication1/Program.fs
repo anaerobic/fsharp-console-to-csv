3dca3ee0ee1f74d4c225ad2054cdd005ded108e8
Astn <austin.w.harris@gmail.com>
2015-03-03 23:55:24 -0700

    A little refactoring

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -10,34 +10,16 @@ let outFile =
     |> List.toArray
     |> Path.Combine
 
-let joinLines (lines : string list) = String.Join(Environment.NewLine, lines)
-let parseToCsvWithHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = true)
-let parseToCsvWithoutHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = false)
-
-let toCsv (lines : string list) = 
-    lines
-    |> joinLines
-    |> parseToCsvWithHeader
-
-let appendAll text = File.AppendAllText(outFile, text)
-let writeAll text = File.WriteAllText(outFile, text)
-let csvAppend (csv : CsvFile) : Unit = csv.SaveToString() |> appendAll
-let csvWriteAll (csv : CsvFile) : Unit = csv.SaveToString() |> writeAll
-
 type consoleReader() = 
     let readEvent = new Event<_>()
     let mutable stop = false
     
     [<CLIEvent>]
-    member this.ReadEvent = readEvent.Publish
+    member this.LineStream = readEvent.Publish
     
     member this.StartReader = 
         try 
             async { 
-                printfn "enter your header row:"
-                Console.ReadLine()
-                |> parseToCsvWithoutHeader
-                |> csvWriteAll
                 while stop = false do
                     Console.ReadLine() |> readEvent.Trigger
             }
@@ -48,23 +30,44 @@ type consoleReader() =
     
     member this.StopReader = stop <- true
 
+let joinLines (lines : string list) = String.Join(Environment.NewLine, lines)
+let parseToCsvWithHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = true)
+let parseToCsvWithoutHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = false)
+
+let appendAll file text = File.AppendAllText(file, text)
+let writeAll file text = File.WriteAllText(file, text)
+let csvAppend file (csv : CsvFile) : Unit = csv.SaveToString() |> appendAll file
+let csvWriteAll file (csv : CsvFile) : Unit = csv.SaveToString() |> writeAll file
+
+let isNullOrEmpty str = 
+    match str with
+       | null | "" -> true
+       | _ -> false
+
+let notEmpty str = not (isNullOrEmpty str)
+
+let writeHeader file = 
+    parseToCsvWithoutHeader
+    >> csvWriteAll file
+
+let appendToFileAsCsv file = 
+    Observable.filter notEmpty
+    >> Observable.map parseToCsvWithHeader
+    >> Observable.subscribe (csvAppend file)
+
 [<EntryPoint>]
 let main argv = 
     let cReader = new consoleReader()
     
     let readerSub = 
-        cReader.ReadEvent
-        |> Observable.bufferCount 2
-        |> Observable.map (fun a -> a |> List.ofSeq)
-        |> Observable.map toCsv
-        |> Observable.subscribe csvAppend
+        cReader.LineStream
+        |> appendToFileAsCsv outFile
     
     let stopSub = 
-        cReader.ReadEvent
-        |> Observable.filter (fun x -> 
-               match x with
-               | null | "" -> true
-               | _ -> false)
+        cReader.LineStream
+        |> Observable.filter isNullOrEmpty
         |> Observable.subscribe (fun x -> cReader.StopReader)
-    
+
+    printfn "enter your header row:"
+    Console.ReadLine() |> writeHeader outFile
     cReader.StartReader; 0 // return an integer exit code
