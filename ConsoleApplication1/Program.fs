722dde4c6f0851cf15f25c9f2805b7f7fa880a02
Michael Martin <mwm.cal@gmail.com>
2015-03-03 20:46:21 -0800

    FSharp.Control.Reactive nupkeg. reactive enhancements by Austin with my header stuff, try...with to break on exception. still an issue that line 1 in buffer isn't validated until Save.

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -1,17 +1,8 @@
 ﻿open System
 open System.IO
 open FSharp.Data
-
-let rec procinput lines callback = 
-    callback lines
-    try 
-        match Console.ReadLine() with
-        | null -> lines
-        | "" -> procinput lines callback
-        | x -> procinput (x :: lines) callback
-    with ex -> 
-        printfn "Nooo! %s" ex.Message
-        []
+open FSharp.Control.Reactive
+open FSharp.Control
 
 let outFile = 
     let desktopFolder = (Environment.GetFolderPath Environment.SpecialFolder.Desktop)
@@ -19,47 +10,61 @@ let outFile =
     |> List.toArray
     |> Path.Combine
 
-let reverseLines lines = List.rev lines
 let joinLines (lines : string list) = String.Join(Environment.NewLine, lines)
-
-let reverseAndJoinLines lines = 
-    lines
-    |> reverseLines
-    |> joinLines
-
-let parseToCsvNoHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = false)
 let parseToCsvWithHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = true)
-let saveCsvToString (csv : FSharp.Data.CsvFile) = csv.SaveToString()
-let wrapInList s = [ s ]
+let parseToCsvWithoutHeader str = FSharp.Data.CsvFile.Parse(str, hasHeaders = false)
 
 let toCsv (lines : string list) = 
     lines
-    |> reverseAndJoinLines
+    |> joinLines
     |> parseToCsvWithHeader
 
-let toCsvAndSave (lines : string list) : Unit = 
-    match lines.Length with
-    | 0 -> ignore 0
-    | _ -> 
-        let csv = toCsv lines
-        csv.Save(outFile, ',', '"')
+let appendAll text = File.AppendAllText(outFile, text)
+let writeAll text = File.WriteAllText(outFile, text)
+let csvAppend (csv : CsvFile) : Unit = csv.SaveToString() |> appendAll
+let csvWriteAll (csv : CsvFile) : Unit = csv.SaveToString() |> writeAll
 
-let rec gogogadget lines = 
-    File.WriteAllLines(outFile, List.toArray lines)
-    match procinput lines toCsvAndSave with
-    | [] -> gogogadget lines
-    | _ -> ignore 0
-
-let addHeaderRow = 
-    printfn "enter your header row:"
-    match Console.ReadLine() with
-    | line -> 
-        line
-        |> parseToCsvNoHeader
-        |> saveCsvToString
-        |> wrapInList
+type consoleReader() = 
+    let readEvent = new Event<_>()
+    let mutable stop = false
+    
+    [<CLIEvent>]
+    member this.ReadEvent = readEvent.Publish
+    
+    member this.StartReader = 
+        try 
+            async { 
+                printfn "enter your header row:"
+                Console.ReadLine()
+                |> parseToCsvWithoutHeader
+                |> csvWriteAll
+                while stop = false do
+                    Console.ReadLine() |> readEvent.Trigger
+            }
+            |> Async.RunSynchronously
+        with ex -> 
+            printfn "Nooo! %s" ex.Message
+            stop <- true
+    
+    member this.StopReader = stop <- true
 
 [<EntryPoint>]
 let main argv = 
-    gogogadget addHeaderRow
-    0 // return an integer exit code
+    let cReader = new consoleReader()
+    
+    let readerSub = 
+        cReader.ReadEvent
+        |> Observable.bufferCount 2
+        |> Observable.map (fun a -> a |> List.ofSeq)
+        |> Observable.map toCsv
+        |> Observable.subscribe csvAppend
+    
+    let stopSub = 
+        cReader.ReadEvent
+        |> Observable.filter (fun x -> 
+               match x with
+               | null | "" -> true
+               | _ -> false)
+        |> Observable.subscribe (fun x -> cReader.StopReader)
+    
+    cReader.StartReader; 0 // return an integer exit code
